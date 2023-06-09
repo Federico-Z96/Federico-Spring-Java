1d53754c628bc7afb75e46939027b38badba4d3d
Zaccaria <zac.fede96@gmail.com>
2023-06-09 10:54:34 +0200

    fine Es2

--- initial/src/main/java/com/example/springboot/Es2/mealsController.java
+++ initial/src/main/java/com/example/springboot/Es2/mealsController.java
@@ -1,13 +1,14 @@
 package com.example.springboot.Es2;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
-
-
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,7 +17,9 @@ public class mealsController {
     public List<Meal> listMeal = Arrays.asList(
             new Meal("Lasagne","piatto tipico Bolognese",13.20),
             new Meal("Pizza","piatto tipico Napoletano",14.60),
+            new Meal("Pizza","piatto tipico",12.50),
             new Meal("Pasta Carbonara","piatto tipico Romano", 12.50));
+
 
 
     @GetMapping("/meal")
@@ -32,6 +35,31 @@ public class mealsController {
                 return meal;
             }
         }return null;
+    }
+
+    @GetMapping("/meal/description-match/{description}")
+    public ResponseEntity <?> GetMealDescription(@PathVariable String description){
+        for (Meal meal : listMeal){
+            if(meal.getDescription().contains(description)){
+                return new ResponseEntity<Meal> (meal, HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>("Error", HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/meal/price")
+    public ResponseEntity <?> getMealPrice(@RequestParam("min") double minPrice, @RequestParam("max") double maxPrice)
+    {
+        List<Meal> mealPriceRange = new ArrayList<>();
+        for (Meal meal : listMeal) {
+            if (meal.getPrice() >= minPrice && meal.getPrice() <= maxPrice) {
+                mealPriceRange.add(meal);
+            }
+        }
+        if (!mealPriceRange.isEmpty()) {
+            return new ResponseEntity<>(mealPriceRange, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Error", HttpStatus.NOT_FOUND);
     }
 
 
--- initial/src/main/java/com/example/springboot/Es3/MealCOntroller.java
+++ initial/src/main/java/com/example/springboot/Es3/MealCOntroller.java
@@ -1,2 +1,22 @@
-package com.example.springboot.Es3;public class MealCOntroller {
+package com.example.springboot.Es3;
+
+import com.example.springboot.Es2.Meal;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MealCOntroller {
+    private static List<Meal> piatti = new ArrayList<Meal>();
+    @PostMapping("/meal")
+    public ResponseEntity <?> AddMeal(@RequestBody Meal newMeal) {
+        piatti.add(newMeal);
+        return new ResponseEntity <String>("aggiunta piatto", HttpStatus.OK);
+    }
+
 }
