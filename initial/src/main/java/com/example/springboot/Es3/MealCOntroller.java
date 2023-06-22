acad14458f75acfc79f9834f1b8588435e6b7f78
Zaccaria <zac.fede96@gmail.com>
2023-06-22 21:04:00 +0200

    completamento es3

--- initial/src/main/java/com/example/springboot/Es3/MealCOntroller.java
+++ initial/src/main/java/com/example/springboot/Es3/MealCOntroller.java
@@ -3,20 +3,61 @@ package com.example.springboot.Es3;
 import com.example.springboot.Es2.Meal;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class MealCOntroller {
+
     private static List<Meal> piatti = new ArrayList<Meal>();
+
+    @GetMapping("/mealList")
+    public List <Meal> getListMeal() {
+        return piatti;
+    }
+
     @PostMapping("/meal")
     public ResponseEntity <?> AddMeal(@RequestBody Meal newMeal) {
         piatti.add(newMeal);
         return new ResponseEntity <String>("aggiunta piatto", HttpStatus.OK);
     }
 
+    @PutMapping("/put/meal/{name}")
+    public ResponseEntity <String> upDateMeal(@PathVariable String name,@RequestBody Meal mealUpdated) {
+        for ( Meal meal : piatti){
+            if (meal.getName().equals(name)) {
+                meal.setName(mealUpdated.getName());
+                meal.setDescription(mealUpdated.getDescription());
+                meal.setPrice(mealUpdated.getPrice());
+                return ResponseEntity.ok("Meal updated");
+            }
+    }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/delete/meal/{name}")
+    public ResponseEntity<String> deleteMeal(@PathVariable String name) {
+        for (Meal meal : piatti) {
+            if (meal.getName().equals(name)) {
+                piatti.remove(meal);
+                return ResponseEntity.ok("Meal deleted");
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+    @DeleteMapping("/delete/meal/price/{price}")
+    public ResponseEntity<String> deleteMealsAbovePrice(@PathVariable double price) {
+        List<Meal> mealsToRemove = new ArrayList<>();
+        for (Meal meal : piatti) {
+            if (meal.getPrice() > price) {
+                mealsToRemove.add(meal);
+            }
+        }
+        piatti.removeAll(mealsToRemove);
+        return ResponseEntity.ok("Meal to remove");
+    }
 }
